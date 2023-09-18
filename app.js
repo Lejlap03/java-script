d1dca1ae694e73025a58252142e861936908c31f
Lejla Prusevic <139994662+Lejlap03@users.noreply.github.com>
2023-09-18 20:31:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,347 @@
+// function parnost_neparnost(number){
+//     if(number===1){
+//         return 'neparan'
+//     }
+//     else if(number===2){
+//         return 'paran'
+//     }
+//     else if(number===3){
+//     return 'neparan'
+//     }
+//     else if(number===4){
+//         return 'paran'
+//     }
+//     else if(number===5){
+//         return 'neparan'
+//     }
+//     else if(number===6){
+//         return 'paran'
+//     }
+//     else if(number===7){
+//         return 'neparan'
+//     }
+//     else if(number===8){
+//         return 'paran'
+//     }
+//     else if(number===9){
+//         return 'neparan'
+//     }
+//     else if(number===10){
+//         return 'paran'
+//     }
+//     else if(number===11){
+//         return 'neparan'
+//     }
+//     else if(number===12){
+//         return 'paran'
+//     }
+//     else if(number===13){
+//     return 'neparan'
+//     }
+//     else if(number===14){
+//         return 'paran'
+//     }
+//     else if(number===15){
+//         return 'neparan'
+//     }
+//     else if(number===16){
+//         return 'paran'
+//     }
+//     else if(number===17){
+//         return 'neparan'
+//     }
+//     else if(number===18){
+//         return 'paran'
+//     }
+//     else if(number===19){
+//         return 'neparan'
+//     }
+//     else if(number===20){
+//         return 'paran'
+//     }
+// }
+//  console.log(parnost_neparnost(8))
+    
+
+// DOMACI
+// let broj=6
+// let broj1=6 
+// let par_nep=(broj%2)
+
+// if(par_nep===0){
+//     console.log( 'paran')
+// }else{
+//     console.log('neparan')
+// }
+
+// if(par_nep===0){
+//     console.log(broj+broj1)
+// }else{
+//     console.log(broj-broj1)
+// }
+
+// tarik
+// function oddOrEven(semafor,arg1,arg2){
+//     if(semafor%2===0){
+//         console.log(arg1+arg2)
+//     }
+// }else{
+//     console.log('arg1-arg2')
+
+
+// oddOrEven()
+
+// console.log(Math.random());
+// let a=5.4
+// console.log(Math.round(a));
+// console.log(Math.floor)
+// Math.ceil
+
+// function logika(semafor1, broj1, broj2){
+//     switch(semafor1){
+//         case 1:
+//             console.log( broj1+broj2);
+//             break;
+//     }
+       
+//  for(let i=0; i<10; i++){
+//     console.log(i);
+//  }
+
+
+//  for(let i= cars.length-1;i>=0; i--){
+//     console.log(cars(i));
+//  }
+
+// let broj = parseInt(prompt('unesite broj'))
+// if(a<0){
+//     console.log('broj je pozitivan')
+// }
+
+// let broj1= parseInt(prompt('unesite prvi broj'))
+// let broj2= parseInt(prompt('unesite drugi broj'))
+
+//  if ( broj1>broj2 ){
+//     console.log('broj je veci')
+//  }
+//  else if( broj1<broj2){
+//     console.log('broj je manji')
+//  }
+//  else( broj1===broj2){
+//     console.log('broj je jednak')
+//  }
+
+// 
+
+
+
+
+// DOMACI 1.1 
+
+// let broj1= parseFloat(prompt('unesite prvi broj'))
+// let broj2= parseFloat(prompt('unesite drugi broj'))
+// let broj3= parseFloat(prompt('unesite treci broj'))
+// let rez;
+
+// rez = broj1
+
+// if(broj2>rez) 
+//  rez=broj2; 
+//  if(broj3>rez) 
+//  rez=broj3; 
+
+//     console.log(rez)
+
+
+
+
+
+// let a= parseFloat(prompt('unesi prvi broj'))
+// let b= parseFloat(prompt('unesi drugu broj'))
+// let c= parseFloat(prompt('unesi treci broj'))
+// ---------------------------------------------------------------------------------------------------------
+ // CAS prvi zadatak domCI
+
+// let broj1= parseFloat(prompt('unesite prvi broj'))
+ // let broj2= parseFloat(prompt('unesite drugi broj'))
+ // let broj3= parseFloatprompt('unesite treci broj'))
+ // let rez;
+ //  rez= broj1;
+
+//  if(rez<broj2){
+ //|rez= broj2
+//  }
+//  if(rez<broj3){
+//     rez=broj3
+//  }
+//.................................................................................
+
+//DRUGI ZADATAK
+
+// let broj1= parseFloat(prompt('unesite prvi broj'))
+// let broj2= parseFloat(prompt('unesite drugi broj'))
+// let broj3= parseFloat(prompt('unesite treci broj'))
+
+// let niz=[broj1,broj2,broj3]
+
+// console.log( niz.sort())
+//***tezi: */
+// let niz[]
+
+// if(broj1<broj2&& broj1<broj3){
+//     niz.push(broj1)
+// }
+// if(broj1<broj2&&broj1>broj3){
+//     niz.push(broj3)
+// }
+
+
+//  let brojBodova=parseFloat(prompt('unesite broj bodova'))
+
+//  if(brojBodova<=50){
+//     console.log('ocena je 5')
+//  }else if(brojBodova<=60){
+//     console.log('ocena je 6')
+//  }else if(brojBodova<=70){
+//     console.log('ocena je 7')
+// }else if(brojBodova<=80){
+//     console.log('ocena je 8')
+//  } else if(brojBodova<=90){
+//     console.log('ocena je 9')
+//  }else{
+//     console.log('ocena je 10')
+//  }
+    
+
+
+// let broj1=parseFloat(prompt('unesi prvi broj'))
+// let broj2=parseFloat(prompt('unesi drugi broj'))
+
+
+
+
+// if(broj1>broj2){
+//     console.log(broj1-broj2)
+// }else{
+//     console.log(broj2-broj1)
+// }
+
+// povrsina
+
+// let a=parseFloat(prompt('unesi prvi broj'))
+// let b=parseFloat(prompt('unesi drugi broj'))
+// let c=parseFloat(prompt('unesi treci broj'))
+
+// let s,p;
+
+// if(a+b>c && a+c>b && b+c>a){
+//   s=(a+b+c/2);
+//   p=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+//   console.log(`povrsina  je' ${p}`);
+// }
+// else{
+//     console.log('ne moze da se napravi trougao')
+// }
+
+// if(a+b>c && a+c>b && b+c>a){
+//       s=(a+b+c/2);
+//       p=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+//       console.log(`povrsina  je' ${p}`);
+//       console.log(` kvadrat od s-a je:` $Math.pow`)
+//     }
+//     else{
+//         console.log('ne moze da se napravi trougao')
+//     }
+
+///////////////////////////FOR PETLJA//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// for(let i=0; i<100; i++){
+//     console.log('pozdrav')
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+// console.log('///////')
+
+// for(let i=1; i<=10; i++){
+//     if(i%2===1){
+//         console.log(i)
+//     }
+// }
+
+
+// for(let i=1; i<=10; i+=2)
+//     if(i%2===1)
+//         console.log(i)
+
+
+
+// for(let i=10;i>=1; i--){
+//     console.log(i)
+// }
+
+//---------------------------------------------------------------------------------------------------------
+// DOMACI 4.14
+
+
+// for(let i=100; i<1000; i++){
+
+//   let prviBroj = Math.floor(i / 100);    
+//   let drugiBroj = Math.floor((i % 100) / 10); 
+//   let treciBroj = i % 10;    
+//   if (drugiBroj === prviBroj + 2 && treciBroj === drugiBroj + 1) {
+//     console.log(i);
+//   }
+ 
+        
+// }
+
+
+        
+        
+
+    
+     
+
+//4.28
+//b
+
+// let n= parseInt(prompt('unesite n:'))
+
+// for( let i=1; i<=n; i++){
+//  for( let j=1; j<=n; j++){
+//     if(i==1 || i==n){
+//         console.log('* ');
+//     }else if(j==1 || j==n){
+//         console.log('* ');
+//     }else{
+//         console.log('');
+//     }
+//  }
+// }
+
+
+
+///4.29(x)
+
+// let n= parseInt(prompt('unesite n:'))
+
+// for( i=0; i<=n; i++){
+//     for(j=0; j<=n; j--){
+//         for(z=0; z<=n; z++){
+//             console.log('*');
+//         }
+//     }
+// }
+// for(i=n-1; i>0; i--){
+//     for(j=n; j>i; j--){
+//         console.log('');
+//     } for(j=0; j<i; j++){
+//         console.log('*');
+//     }
+// }
+
+
+
+//4.30
---
+++ set.js
@@ -0,0 +1,306 @@
+/////4.15
+
+// for(let i=100; i<1000;i++){
+//   let prviBroj = Math.floor(i / 100);    
+//   let drugiBroj = Math.floor((i % 100) / 10); 
+//   let treciBroj = i % 10; 
+
+//   if(Math.pow(prviBroj,3)+Math.pow(drugiBroj,3)+Math.pow(treciBroj,3)===i)
+//   console.log(i);
+// }
+
+
+
+
+//4.16
+
+// for(let i=100; i<1000;i++){
+
+//     // let prviBroj = Math.floor(i / 100);    
+//     // let drugiBroj = Math.floor((i % 100) / 10); 
+
+//     //pow stepen
+//     //uzimanje prve dve cifre
+//     let dveCifre = Math.floor(i/100)
+//     let treciBroj = i % 10; 
+
+
+//     console.log(i,dveCifre);
+
+//     if(Math.pow(dveCifre,2)-Math.pow(treciBroj,2)===i){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+//4.17
+
+// for(let i=1; a<=9; a++){
+//     for(let b=0; b<=9; b++){
+//         for( let c=0; c<=0; c++)
+//         broj=a*100+b*10+c;
+//         let dveCifre= a*10+c;
+
+//         if(broj%dveCifre===0){
+//             console.log(broj);
+//         }
+//     }
+    
+// }
+
+
+
+
+
+//4.18
+
+// let n= 0;
+// let suma=0;
+
+// for(let b=0; b<=9; b++)
+//     for(let b=0; b<=9; b++){
+//                 for( let c=0; c<=0; c++)
+//                 broj=a*100+b*10+c;
+//                 let dveCifre= a*10+c;
+        
+//                 if(a+b+c===5){
+//                     broj+=a*100+b
+//                 }
+//             }
+
+
+
+
+
+//4.19
+
+// let broj = parseInt(prompt('unesite broj'))
+
+// for(let i=1; i<=broj; i++){
+//     if(broj%i===0){
+//         console.log(i);
+//     }
+//}
+
+
+
+//4.20
+
+
+// let broj = parseInt(prompt('unesite broj'))
+
+// let suma=0;
+
+// for(let i=1; i<broj; i++){
+//         if(broj%i===0)
+//         {
+//             suma +=i;
+//             console.log(i);
+//         }
+// }
+
+// if(suma==broj){
+//     console.log(' broj deljiv je',broj);
+// }else{
+//     console.log('broj nije deljiv');
+// }
+
+//4.21
+
+// let broj = parseInt(prompt('unesite broj ucenika:'))
+// suma=0;
+// let jedinica=0;
+
+//  for(let i=1; i<=broj; i++){
+//    let ocena =  parseInt(prompt('unesite ocenu',i ))
+//    suma+=ocena;
+// if(ocena===1){
+//     jedinica++
+// }
+//  }
+// console.log(`prosecna ocena je ${suma/n}`);
+// console.log(`prosecna ocena je ${jedinica je}`);
+
+
+
+
+//4.22
+
+// let broj = parseInt(prompt('unesite broj sati:'));
+
+
+// for(let sat=0; sat<=23; sat++);
+// for(let minuti=0; minuti<=59; minuti++);
+// for(let sekunda=0;sekunda<=59; sekunda++);
+
+// if(broj=={sat/10+sat%10+min/10+sec%10}){
+
+
+
+// }
+/////////////////////////////////////// D O M A C I /////////////////////////////////////////////////////////////////////////////////////
+
+
+//4.26
+
+
+// let n = parseInt(prompt('Unesite n:'));
+
+// for(let i=0; i<n; i++){
+//     for(let j=0; j<n; j++){
+//        console.log('*');
+//        console.log('\n'); //jedan prazan red
+       
+//     }
+// }
+
+
+
+
+//b
+
+
+// let n = parseInt(prompt('Unesite n:'));
+
+
+// for(let i=0; i<n; i++){
+//         for(let j=0; j<n*2; j++){
+//            console.log('*');
+//         }
+// }
+
+
+
+//4.27
+
+// let n= parseInt(prompt('unesite n:'));
+// let m= parseInt(prompt('unesite m:'));
+
+//  for(let i=0; i<n; i++){
+//         for(let j=0; j<m; j++){
+//             console.log(j);
+//         }
+//         console.log('\n');
+//  }
+
+
+//4.28
+
+
+// let n = parseInt(prompt('Unesite n:'));
+
+// for(let i=1; i<=n; i++){
+//     for(let j=1; j<=n; j++){
+//        console.log('*');
+//        console.log('\n');
+//     }
+// }
+
+
+
+
+
+//b
+
+// let n = parseInt(prompt('Unesite n:'));
+
+// for(let i=1; i<=n; i++){
+//     for(let j=1; j<=n; j++){
+//         console.log('*');
+
+//         console.log('\n');
+//     }
+// }
+
+
+
+
+//d
+
+
+// let n = parseInt(prompt('Unesite n:'));
+
+// for( let i=1; i<=n; i++){
+//     for( let j=1; j<=n; j--)
+// }
+
+
+
+
+
+//b  (4.28)
+
+// let n = parseInt(prompt('Unesite broj redova:'));
+// let m = parseInt(prompt('Unesite broj kolona:'));
+
+
+// for(let i=1; i<=n; i++){
+//     let rowOutput= '';
+//     for(let i===1 ||  i===n){
+//         if(i===1|| i===n){
+//             rowOutput +='*'
+//         }
+//     }else 
+// }
+
+
+
+//v
+
+// let n = parseInt(prompt('Unesite broj redova:'));
+// let m = parseInt(prompt('Unesite broj kolona:'));
+
+
+
+// for( let i=1; i<=n; i++){
+//     let rowOutput=''
+//    for(let j=1; j<)
+// }
+
+
+
+
+
+
+// let n = parseInt(prompt('Unesite broj redova:'));
+// let m = parseInt(prompt('Unesite broj kolona:'));
+
+// for(let i=1; i<=n; i++){
+//     let rowOutput = ''
+//     for(j=1; j<=i; j++){
+//         if(i==n  ){
+//             rowOutput +='*';
+//         }else{
+//             rowOutput=''
+//         }
+//     }
+// }
+// console.log(rowOutput);
+// console.log('\n');
+
+
+
+
+
+
+
+
+
+//4.29 domaci
+
+// let n= parseInt(prompt('unesite n:'));
+
+// for(let i=0; i<=n; i++){
+//     for(let j=0; j<=n; j++){
+//         for(let z=0; z<0; z++)
+//     }
+// }
+
+
+
+// for(let i=0; i<n; i++){
+//     let rowInput = '';
+
