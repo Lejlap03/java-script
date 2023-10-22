ce2a89361393acd88e5d15c90221ba47e0ecc939
Lejla Prusevic <139994662+leylaprusevic@users.noreply.github.com>
2023-10-22 12:45:05 +0200

    break

---
+++ break.js
@@ -0,0 +1,52 @@
+////........................BREAK......................
+
+
+
+// let broj = parseInt(prompt('unesite brojeve'))
+
+// let stop=0
+
+// do{
+//     broj =  parseInt(prompt('unesite brojeve'))
+//     console.log(broj);
+//     if(broj===0|| broj===100) break;
+//     i++
+// }
+// while(true)
+///////////////////////////////////////////////////////////////
+///7.3
+
+// let n;
+// let output='';
+
+// for(let i=0; i<100; i++){
+// n=  parseInt(prompt('unesite broj'));
+//     if(n===0)
+//      break;
+//     output +=   `${n},`;
+// }
+// console.log(output);
+
+////////////////////////////////////////////////////////////
+// for( let i=1; i<=500; i++){
+
+//     if(i%3===0 && i%4===0 && i%5===0 && i%7===0){
+//     console.log(i);
+//     break;
+// }
+// }
+
+
+////////////////////////////////////////////////////////////
+//7.7
+
+// for(let i=1; i<=100; i++){ 
+//     let j=2;
+//     for(; j<i; j++) {
+//         if(i%j===0)
+
+//             break;
+//         }
+//         if(i===j)
+//         console.log(i);
+//     }
